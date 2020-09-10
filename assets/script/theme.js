14f4fabe3f9ce4fb8cd1b93e920e16aca32508b2
Aradea Atfal Risdianto <aradeaa9@gmail.com>
2020-09-10 17:18:28 +0700

    Update

--- assets/script/theme.js
+++ assets/script/theme.js
@@ -1,10 +1,12 @@
 jQuery(document).ready(function ($) {
   $(window).scroll(function () {
-    if ($(document).scrollTop() > 600) {
+    if ($(document).scrollTop() > 350) {
       $(".navbar").removeClass("fadeIn");
+      $("body").addClass("shrink");
       $(".navbar").addClass("animated fadeInDown");
     } else {
       $(".navbar").removeClass("fadeInDown");
+      $("body").removeClass("shrink");
       $(".navbar").addClass("animated fadeIn");
     }
   });
